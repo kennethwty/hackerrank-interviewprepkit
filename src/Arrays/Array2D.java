57e93d10346862369c5f2bf452e31fb2159f6ccd
kennethwty <kennethwty93@gmail.com>
2018-09-08 18:07:05 -0400

    Upload Arrays: 2D Array - Hourglass

---
+++ src/Arrays/Array2D.java
@@ -0,0 +1,71 @@
+package Arrays;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+/*
+    By: Tszyan "Kenneth" Wong
+
+    Link to the problem: https://www.hackerrank.com/challenges/2d-array/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+
+    Sample Input:
+
+        1 1 1 0 0 0
+        0 1 0 0 0 0
+        1 1 1 0 0 0
+        0 0 2 4 4 0
+        0 0 0 2 0 0
+        0 0 1 2 4 0
+
+    Sample Output:
+
+        19
+ */
+public class Array2D {
+    static int hourglassSum(int[][] arr) {
+        int sum;
+        int max = Integer.MIN_VALUE;
+
+        // i = row; j = column
+        for(int i = 0; i < arr.length-2; i++) {
+            for(int j = 0; j < arr.length-2; j++) {
+                sum = arr[i][j] + arr[i][j+1] + arr[i][j+2]
+                        + arr[i+1][j+1]
+                        + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+
+                if(sum > max) max = sum;
+            }
+        }
+
+        return max;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        int result = hourglassSum(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
